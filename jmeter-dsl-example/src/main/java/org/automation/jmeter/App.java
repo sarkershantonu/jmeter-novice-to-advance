8318da7952dd5cf40d4175dbc728362fd9693c25
shantonu sarker <sarker.shantonu@gmail.com>
2022-05-26 23:50:07 -0500

    Jmeter DSL Example: first exection

--- jmeter-dsl-example/src/main/java/org/automation/jmeter/App.java
+++ jmeter-dsl-example/src/main/java/org/automation/jmeter/App.java
@@ -1,10 +1,23 @@
 package org.automation.jmeter;
 
+import us.abstracta.jmeter.javadsl.core.TestPlanStats;
+
+import java.io.IOException;
+import java.time.Instant;
+
+import static us.abstracta.jmeter.javadsl.JmeterDsl.*;
+
 /**
  * @author Shantonu Sarker on 5/26/2022
  */
 public class App {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        TestPlanStats stats = testPlan(
+                threadGroup("DemoExecution",10,50,
+                        httpSampler("https://jmeter.apache.org/")
+                ),
+                jtlWriter("demoExecution"+ Instant.now().toString().replace(":","_")+".jtl")
+        ).run();
+        System.out.println(stats.overall().sampleTimePercentile99().toString());
     }
 }
