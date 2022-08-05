5e0c307b518eed0830ad0df2fd1d8f4ed98c970e
shantonu sarker <sarker.shantonu@gmail.com>
2022-08-05 00:15:19 -0500

    refactored

--- jmeter-dsl-example/src/main/java/org/automation/jmeter/examples/junit4/ThreadGroupExample.java
+++
@@ -1,8 +0,0 @@
-package org.automation.jmeter.examples.junit4;
-
-import org.automation.jmeter.core.Junit4PerformanceTest;
-
-public class ThreadGroupExample extends Junit4PerformanceTest {
-
-
-}
---
+++ jmeter-dsl-example/src/test/java/org/automation/jmeter/tests/junit4/ThreadGroupExample.java
@@ -0,0 +1,24 @@
+package org.automation.jmeter.tests.junit4;
+
+import org.automation.jmeter.core.Junit4PerformanceTest;
+import org.junit.Assert;
+import org.junit.Test;
+import us.abstracta.jmeter.javadsl.core.TestPlanStats;
+
+import java.io.IOException;
+
+import static us.abstracta.jmeter.javadsl.JmeterDsl.*;
+
+public class ThreadGroupExample extends Junit4PerformanceTest {
+
+    @Test
+    public void testThreadGroup() throws IOException {
+        TestPlanStats report = testPlan(
+                threadGroup("Checking Default Thread group", 5, 5,
+                        httpSampler("https://bratahome.duckdns.org")))
+                .run();
+        //Assert.assertTrue("fails : Should be less than 5s",(report.overall().sampleTimePercentile99().getSeconds())>=5L);
+
+        Assert.assertTrue("Should be less than 5s",(report.overall().sampleTimePercentile99().getSeconds())<=5L);
+    }
+}
