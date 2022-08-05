78749559c4a9d10d7bb3b5b2e5b6692df1187c88
shantonu sarker <sarker.shantonu@gmail.com>
2022-08-04 23:54:46 -0500

    refactor: organize based on example

--- jmeter-dsl-example/src/main/java/org/automation/jmeter/core/Junit4PerformanceTest.java
+++ jmeter-dsl-example/src/main/java/org/automation/jmeter/core/Junit4PerformanceTest.java
@@ -3,5 +3,5 @@ package org.automation.jmeter.core;
 /**
  * @author Shantonu Sarker on 5/26/2022
  */
-public class BaseJunit4 extends TestBase{
+public abstract class Junit4PerformanceTest extends TestBase {
 }
