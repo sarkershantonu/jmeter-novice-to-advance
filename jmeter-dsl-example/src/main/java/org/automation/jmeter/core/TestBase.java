9a428aabf12e945bbbe642328c336bc5b193f9b0
shantonu sarker <sarker.shantonu@gmail.com>
2022-08-05 00:15:03 -0500

    include JTL folder naming

--- jmeter-dsl-example/src/main/java/org/automation/jmeter/core/TestBase.java
+++ jmeter-dsl-example/src/main/java/org/automation/jmeter/core/TestBase.java
@@ -1,8 +1,13 @@
 package org.automation.jmeter.core;
 
+import java.time.Instant;
+
 /**
  * @author Shantonu Sarker on 5/26/2022
  */
 public abstract class TestBase {
-
+    final String jjtl_folder="target/";
+    public final String getJtlFolderName(){
+        return jjtl_folder+"demoExecution"+ Instant.now().toString().replace(":","_")+".jtl";
+    }
 }
