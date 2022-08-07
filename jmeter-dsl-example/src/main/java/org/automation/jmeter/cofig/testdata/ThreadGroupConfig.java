3663bde74c52d8dbe11288ffd1717b100e8b6fa6
shantonu sarker <sarker.shantonu@gmail.com>
2022-08-07 01:32:29 -0500

    introducing data loader of config

--- jmeter-dsl-example/src/main/java/org/automation/jmeter/cofig/testdata/ThreadGroupConfig.java
+++ jmeter-dsl-example/src/main/java/org/automation/jmeter/cofig/testdata/ThreadGroupConfig.java
@@ -1,4 +1,11 @@
 package org.automation.jmeter.cofig.testdata;
 
 public class ThreadGroupConfig {
+    private long threadCount;
+    private long rampUp;
+    private long loopCount;
+    private boolean isInifinite;
+    private long duration;
+    private long startupDelay;
+
 }
