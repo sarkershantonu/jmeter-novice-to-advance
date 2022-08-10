18279e014580d15a7cde3cd34326dc7c1d7fd13a
shantonu sarker <sarker.shantonu@gmail.com>
2022-08-10 06:48:16 -0500

    jmeter component config

--- jmeter-dsl-example/src/main/java/org/automation/jmeter/cofig/testdata/HttpRequestSamplerConf.java
+++ jmeter-dsl-example/src/main/java/org/automation/jmeter/cofig/testdata/HttpRequestSamplerConf.java
@@ -1,14 +1,7 @@
 package org.automation.jmeter.cofig.testdata;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 
-@Getter
-@Setter
-@ToString
-public class HttpRequestSamplerConf {
-private String name;
-private String comments;
+public class HttpRequestSamplerConf extends JmeterComponent{
 private HttpHeaderConf headerConfig;
+
 }
