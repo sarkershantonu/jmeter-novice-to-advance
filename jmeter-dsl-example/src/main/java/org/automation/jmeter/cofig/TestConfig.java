8aac727372b35755e255dbcaa0f1a650b81c7e4c
shantonu sarker <sarker.shantonu@gmail.com>
2022-08-13 23:37:50 -0500

    loading http

--- jmeter-dsl-example/src/main/java/org/automation/jmeter/cofig/TestConfig.java
+++ jmeter-dsl-example/src/main/java/org/automation/jmeter/cofig/TestConfig.java
@@ -1,10 +1,22 @@
 package org.automation.jmeter.cofig;
 
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * @author Shantonu Sarker on 5/27/2022
  */
 public class TestConfig {
     public static String protocol;
     public static String HOST;
+    public static Properties httpData;
+
+    static {
+        try {
+            httpData = new ContextLoader("http.ini").loadFromFolder("test-data");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
