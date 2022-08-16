2cad145c88fe846f8d9b6dfb2f5b0fecd1799aca
shantonu sarker <sarker.shantonu@gmail.com>
2022-08-15 23:33:29 -0500

    thread group examples

--- jmeter-dsl-example/src/main/java/org/automation/jmeter/steps/DefaultThreadGroupExample.java
+++ jmeter-dsl-example/src/main/java/org/automation/jmeter/steps/DefaultThreadGroupExample.java
@@ -1,12 +1,26 @@
 package org.automation.jmeter.steps;
 
+import org.junit.Assert;
+import us.abstracta.jmeter.javadsl.core.TestPlanStats;
+
+import java.io.IOException;
 import java.util.Properties;
 
+import static us.abstracta.jmeter.javadsl.JmeterDsl.*;
+
 public class DefaultThreadGroupExample {
-    private Properties threadProperties;
 
-    public DefaultThreadGroupExample(Properties threadProperties) {
-        this.threadProperties = threadProperties;
+    //todo
+    public void runWithProperties(Properties properties) throws IOException {
+        TestPlanStats report = testPlan(
+                threadGroup(properties.getProperty("comments"),
+                        Integer.valueOf(properties.getProperty("comments")),
+                        Integer.valueOf(properties.getProperty("comments")),
+                        httpSampler("https://bratahome.duckdns.org")))
+                .run();
+        //Assert.assertTrue("fails : Should be less than 5s",(report.overall().sampleTimePercentile99().getSeconds())>=5L);
+
+        Assert.assertTrue("Should be less than 5s",(report.overall().sampleTimePercentile99().getSeconds())<=5L);
+
     }
-
 }
