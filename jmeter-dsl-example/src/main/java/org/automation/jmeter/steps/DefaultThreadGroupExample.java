96f253a6b151c8d66e3a81181342dff746ff94eb
shantonu sarker <sarker.shantonu@gmail.com>
2022-08-15 23:28:18 -0500

    thread group examples

---
+++ jmeter-dsl-example/src/main/java/org/automation/jmeter/steps/DefaultThreadGroupExample.java
@@ -0,0 +1,12 @@
+package org.automation.jmeter.steps;
+
+import java.util.Properties;
+
+public class DefaultThreadGroupExample {
+    private Properties threadProperties;
+
+    public DefaultThreadGroupExample(Properties threadProperties) {
+        this.threadProperties = threadProperties;
+    }
+
+}
