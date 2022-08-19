f05b25166153553bef26d206fac19c321c4c4aab
shantonu sarker <sarker.shantonu@gmail.com>
2022-08-19 00:11:51 -0500

    thread plan creation

--- jmeter-dsl-example/src/main/java/org/automation/jmeter/steps/threads/DefaultThreadPlan.java
+++ jmeter-dsl-example/src/main/java/org/automation/jmeter/steps/threads/DefaultThreadPlan.java
@@ -2,6 +2,7 @@ package org.automation.jmeter.steps.threads;
 
 import us.abstracta.jmeter.javadsl.core.DslTestPlan;
 
+import java.time.Duration;
 import java.util.Properties;
 
 import static us.abstracta.jmeter.javadsl.JmeterDsl.*;
@@ -10,6 +11,8 @@ public class DefaultThreadPlan {
 
     private Properties threadProperties;
     private Properties http;
+
+
 
     public DefaultThreadPlan(Properties threadGroupData, Properties httpData) {
         this.threadProperties = threadGroupData;
@@ -27,10 +30,11 @@ public class DefaultThreadPlan {
                         httpSampler(http.getProperty("BASE_UIRL"))));
     }
 
-    private DslTestPlan getPlan(final String name, final int threads, final int rampup, final int loop, final Properties http){
-        return testPlan(threadGroup(name, threads, rampup, httpSampler(http.getProperty("BASE_UIRL"))));
+    private DslTestPlan getPlan(final String name, final int threads, final Duration rampup, final int iteration, final Properties http){
+        return testPlan(threadGroup(name, threads, iteration, httpSampler(http.getProperty("BASE_UIRL"))).rampTo(threads,rampup));
     }
-    private DslTestPlan getPlan(final String name, final int threads, final int rampup, final int duration, final int startupDelay, final Properties http){
+    private DslTestPlan getPlan(final String name, final int threads, final int rampup, final Duration duration, final int startupDelay, final Properties http){
+        return testPlan(threadGroup(name, threads, duration, httpSampler(http.getProperty("BASE_UIRL"))));
+    }
 
-    }
 }
