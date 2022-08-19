4eadd43ec21e31d00f3391e813ac8315fab36d0b
shantonu sarker <sarker.shantonu@gmail.com>
2022-08-18 23:53:49 -0500

    thread plan creation

---
+++ jmeter-dsl-example/src/main/java/org/automation/jmeter/steps/threads/DefaultThreadPlan.java
@@ -0,0 +1,29 @@
+package org.automation.jmeter.steps.threads;
+
+import us.abstracta.jmeter.javadsl.core.DslTestPlan;
+
+import java.util.Properties;
+
+import static us.abstracta.jmeter.javadsl.JmeterDsl.*;
+
+public class DefaultThreadPlan {
+
+    private Properties threadProperties;
+    private Properties http;
+
+    public DefaultThreadPlan(Properties threadGroupData, Properties httpData) {
+        this.threadProperties = threadGroupData;
+        this.http = httpData;
+    }
+
+    public DslTestPlan getPlanWithIteration() {
+        final String name = threadProperties.getProperty("name");
+        final int threadCount = Integer.valueOf(threadProperties.getProperty("thread"));
+        final int rampup = Integer.valueOf(threadProperties.getProperty("rampup"));
+        final String erroraction = threadProperties.getProperty("erroraction");
+
+        return testPlan(
+                threadGroup(name, threadCount, rampup,
+                        httpSampler(http.getProperty("BASE_UIRL"))));
+    }
+}
