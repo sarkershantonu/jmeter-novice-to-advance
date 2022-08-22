61da907cd2067fecdd556e5af0ba92743ac7cf8d
shantonu sarker <sarker.shantonu@gmail.com>
2022-08-21 23:15:38 -0500

    saving test plan JXM

--- jmeter-dsl-example/src/main/java/org/automation/jmeter/steps/TestPlanCreator.java
+++ jmeter-dsl-example/src/main/java/org/automation/jmeter/steps/TestPlanCreator.java
@@ -1,4 +1,22 @@
 package org.automation.jmeter.steps;
 
+import org.automation.jmeter.cofig.JavaProperties;
+import us.abstracta.jmeter.javadsl.core.DslTestPlan;
+
+import java.io.IOException;
+
 public class TestPlanCreator {
+    private final DslTestPlan testPlan;
+
+    public TestPlanCreator(final DslTestPlan testPlan) {
+        this.testPlan = testPlan;
+    }
+
+    public void saveTestPlan(final String name) {
+        try {
+            testPlan.saveAsJmx("JMX" + JavaProperties.FILE_SEPARATOR + name + ".jmx");
+        } catch (IOException e) {
+            throw new RuntimeException("Cant Save File", e);
+        }
+    }
 }
