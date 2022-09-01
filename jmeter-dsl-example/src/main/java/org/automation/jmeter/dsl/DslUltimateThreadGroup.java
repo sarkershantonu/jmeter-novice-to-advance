d688a30c6766cb1bd734b48cd8f63ca75339a8e7
shantonu sarker <sarker.shantonu@gmail.com>
2022-08-31 23:58:06 -0500

    target default thread group

---
+++ jmeter-dsl-example/src/main/java/org/automation/jmeter/dsl/DslUltimateThreadGroup.java
@@ -0,0 +1,18 @@
+package org.automation.jmeter.dsl;
+
+import org.apache.jmeter.gui.JMeterGUIComponent;
+import org.apache.jmeter.threads.AbstractThreadGroup;
+import us.abstracta.jmeter.javadsl.core.threadgroups.BaseThreadGroup;
+
+import java.util.List;
+
+public class DslUltimateThreadGroup extends BaseThreadGroup<DslUltimateThreadGroup> {
+    protected DslUltimateThreadGroup(String name, Class<? extends JMeterGUIComponent> guiClass, List<ThreadGroupChild> children) {
+        super(name, guiClass, children);
+    }
+
+    @Override
+    protected AbstractThreadGroup buildThreadGroup() {
+        return null;
+    }
+}
