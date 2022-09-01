7099691da446f1d5267464547f3f698d028e3cdb
shantonu sarker <sarker.shantonu@gmail.com>
2022-09-01 00:06:31 -0500

    stepping thread Added

---
+++ jmeter-dsl-example/src/main/java/org/automation/jmeter/dsl/threadgroups/DslSteppingThreadGroup.java
@@ -0,0 +1,18 @@
+package org.automation.jmeter.dsl.threadgroups;
+
+import org.apache.jmeter.gui.JMeterGUIComponent;
+import org.apache.jmeter.threads.AbstractThreadGroup;
+import us.abstracta.jmeter.javadsl.core.threadgroups.BaseThreadGroup;
+
+import java.util.List;
+
+public class DslSteppingThreadGroup extends BaseThreadGroup<DslSteppingThreadGroup> {
+    protected DslSteppingThreadGroup(String name, Class<? extends JMeterGUIComponent> guiClass, List<ThreadGroupChild> children) {
+        super(name, guiClass, children);
+    }
+
+    @Override
+    protected AbstractThreadGroup buildThreadGroup() {
+        return null;
+    }
+}
