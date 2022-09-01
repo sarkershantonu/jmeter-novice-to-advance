028d9f7388029158b57c975effe705e87438df09
shantonu sarker <sarker.shantonu@gmail.com>
2022-09-01 00:16:53 -0500

    Ultimate thread group

--- jmeter-dsl-example/src/main/java/org/automation/jmeter/dsl/threadgroups/DslUltimateThreadGroup.java
+++ jmeter-dsl-example/src/main/java/org/automation/jmeter/dsl/threadgroups/DslUltimateThreadGroup.java
@@ -2,8 +2,12 @@ package org.automation.jmeter.dsl.threadgroups;
 
 import org.apache.jmeter.gui.JMeterGUIComponent;
 import org.apache.jmeter.threads.AbstractThreadGroup;
+import org.automation.jmeter.dsl.model.UltimateThreadGroupRow;
 import us.abstracta.jmeter.javadsl.core.threadgroups.BaseThreadGroup;
+import us.abstracta.jmeter.javadsl.core.threadgroups.defaultthreadgroup.Stage;
+import us.abstracta.jmeter.javadsl.core.threadgroups.defaultthreadgroup.UltimateThreadGroupHelper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DslUltimateThreadGroup extends BaseThreadGroup<DslUltimateThreadGroup> {
@@ -11,8 +15,23 @@ public class DslUltimateThreadGroup extends BaseThreadGroup<DslUltimateThreadGro
         super(name, guiClass, children);
     }
 
+    private List<UltimateThreadGroupRow> config;
+
+    public DslUltimateThreadGroup setLoadPlan(List<UltimateThreadGroupRow> config) {
+        this.config = config;
+        return this;
+    }
+
     @Override
     protected AbstractThreadGroup buildThreadGroup() {
-        return null;
+        List<Stage> stages = new ArrayList<>();
+        for(UltimateThreadGroupRow aRow : this.config){
+            stages.add(aRow.getStage());
+        }
+        return new UltimateThreadGroupHelper(stages).buildThreadGroup();
+    }
+
+    public DslUltimateThreadGroup saveLoadPlanAsImage(final String imageName){
+        return this;
     }
 }
