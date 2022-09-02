c153778726817c2f158da987ebc0274c71a5c306
shantonu sarker <sarker.shantonu@gmail.com>
2022-09-02 01:35:29 -0500

    ultimate td

--- jmeter-dsl-example/src/main/java/org/automation/jmeter/dsl/threadgroups/DslSteppingThreadGroup.java
+++ jmeter-dsl-example/src/main/java/org/automation/jmeter/dsl/threadgroups/DslSteppingThreadGroup.java
@@ -3,13 +3,20 @@ package org.automation.jmeter.dsl.threadgroups;
 import org.apache.jmeter.gui.JMeterGUIComponent;
 import org.apache.jmeter.threads.AbstractThreadGroup;
 import us.abstracta.jmeter.javadsl.core.threadgroups.BaseThreadGroup;
+import us.abstracta.jmeter.javadsl.core.threadgroups.DslDefaultThreadGroup;
+import us.abstracta.jmeter.javadsl.core.threadgroups.defaultthreadgroup.UltimateThreadGroupHelper;
 
 import java.util.List;
 
-public class DslSteppingThreadGroup extends BaseThreadGroup<DslSteppingThreadGroup> {
+public class DslSteppingThreadGroup extends BaseThreadGroup<DslDefaultThreadGroup> {
+
+    private UltimateThreadGroupHelper helper ;
+
     protected DslSteppingThreadGroup(String name, Class<? extends JMeterGUIComponent> guiClass, List<ThreadGroupChild> children) {
         super(name, guiClass, children);
+
     }
+
 
     @Override
     protected AbstractThreadGroup buildThreadGroup() {
