4777dee21d1f6f096c3fd2c7e7f464e806530da5
shantonu sarker <sarker.shantonu@gmail.com>
2022-09-10 23:55:39 -0500

    regex extractor added

---
+++ jmeter-dsl-example/src/main/java/org/automation/jmeter/steps/postprocessors/JmeterRegex.java
@@ -0,0 +1,14 @@
+package org.automation.jmeter.steps.postprocessors;
+
+import org.automation.jmeter.steps.JmeterElementBase;
+
+import java.util.Properties;
+
+/***
+ * Regular Expression Extractor
+ */
+public class JmeterRegex extends JmeterElementBase {
+    public JmeterRegex(Properties data) {
+        super(data);
+    }
+}
