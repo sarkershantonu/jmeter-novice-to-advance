3899f24c5952e641884cff59ba216f686eadc22c
shantonu sarker <sarker.shantonu@gmail.com>
2022-09-18 22:56:39 -0500

    simple thread group

--- jmeter-dsl-example/src/main/java/org/automation/jmeter/tests/scripts/LoadScripts.java
+++ jmeter-dsl-example/src/main/java/org/automation/jmeter/tests/scripts/LoadScripts.java
@@ -1,4 +1,17 @@
 package org.automation.jmeter.tests.scripts;
 
+import us.abstracta.jmeter.javadsl.core.TestPlanStats;
+
+import java.io.IOException;
+
+import static us.abstracta.jmeter.javadsl.JmeterDsl.*;
+
 public class LoadScripts {
+
+    public static TestPlanStats simpleThreadGroupExample() throws IOException {
+        return testPlan(
+                threadGroup("Checking Default Thread group", 5, 5,
+                        httpSampler("https://bratahome.duckdns.org")))
+                .run();
+    }
 }
