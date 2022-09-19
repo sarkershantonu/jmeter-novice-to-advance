8e16cfde63a5dd54c0635d6cf0643f83e9944733
shantonu sarker <sarker.shantonu@gmail.com>
2022-09-18 23:00:33 -0500

    Test NG running tests

--- jmeter-dsl-example/src/main/java/org/automation/jmeter/tests/testng/ThreadGroupExample.java
+++ jmeter-dsl-example/src/main/java/org/automation/jmeter/tests/testng/ThreadGroupExample.java
@@ -1,5 +1,19 @@
 package org.automation.jmeter.tests.testng;
 
 
-public class ThreadGroupExample  {
+import org.automation.jmeter.tests.scripts.LoadScripts;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import us.abstracta.jmeter.javadsl.core.TestPlanStats;
+
+import java.io.IOException;
+
+public class ThreadGroupExample {
+
+    @Test
+    public void testForThreadGroup() throws IOException {
+        TestPlanStats report = LoadScripts.simpleThreadGroupExample().run();
+        Assert.assertTrue(report.overall().sampleTimePercentile99().getSeconds()<=5L, "should be less than 5s");
+
+    }
 }
